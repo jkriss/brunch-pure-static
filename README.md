0d2d93690ff39b0af14bb817c05c12b3fa979193
Jesse Kriss <jesse@jklabs.net>
2015-12-30 13:16:52 -0800

    Hooking in to brunch-static-tools, making the server work

--- README.md
+++ README.md
@@ -18,9 +18,9 @@ Start a new Brunch project using this skeleton:
 
 Start a development server and watch for changes:
 
-`brunch watch`
+`brunch watch --server`
 
-The server will start at the first open port starting at 3000.
+Load it up at [http://localhost:3333](http://localhost:3333).
 
 ## Development server
 
@@ -29,3 +29,26 @@ This means you can use paths like `/about` in your pages, and it will load
 `about.html`.
 
 This is handy if you deploy to S3 with stripped suffixes.
+
+## Building for production
+
+If you use the `--production` flag with `brunch build` or `brunch watch`,
+the brunch-static-tools plugin will run, and will do the following:
+
+- generate fingerprints for your static asset
+- strip .html from your html file names
+- rewrite your html pages to use the new asset and html files paths
+- gzip everything
+
+This will set you up for deploying to S3, but don't forget to set the
+'Content-Type' and 'Content-Encoding' headers accordingly.
+
+## Deploying to S3
+
+If you want to get fancy, set up an S3 bucket for website hosting, then change
+the following line in `package.json` to match your S3 bucket name:
+
+`"_s3BucketName" : "example-brunch-static-site",`
+
+Once you've done this, you can run `npm run deploy` to build for production and
+upload to S3 in one shot.
--- brunch-server.js
+++ brunch-server.js
@@ -1,18 +1,37 @@
 var express = require('express')
 var compression = require('compression')
 var serveStatic = require('serve-static')
-var op = require('openport');
-
+var Path = require('path');
+var readChunk = require('read-chunk');
+var GzipMagic = new Buffer([0x1f, 0x8b]); // gzip files start with this
 var app = express();
 
-app.use(compression());
-app.use(serveStatic('public', {
-  extensions: ['html']
-}))
+module.exports = function(port, path, callback) {
 
-op.find({ startingPort: 3000, endingPort: 3999 }, function(err, port) {
-    if(err) { console.log(err); return; }
-    app.listen(port);
-    console.log(`Brunch development server running at http://localhost:${port}`)
+  app.use(compression());
+  app.use(serveStatic(path, {
+    extensions: ['html'],
+    setHeaders: setHeaders
+  }))
+
+  function setHeaders(res, path) {
+    if (Path.extname(path) === '') {
+      res.setHeader('Content-Type', 'text/html')
+    }
+    // if it's fingerprinted, use that as the etag
+    if (Path.basename(path).match(/[0-9a-f]{32}-/)) {
+      var hash = Path.basename(path).split('-')[0];
+      res.setHeader('Etag', '"'+hash+'"');
+      res.setHeader('Cache-Control', 'public, max-age=2592000'); // 1 month
+    }
+    // if it's already a gzip file (based on magic number) then say so
+    var magicNumber = readChunk.sync(path, 0, 2);
+    if (magicNumber.equals(GzipMagic)) {
+      res.setHeader('Content-Encoding', 'gzip')
+    }
   }
-);
+
+  app.listen(port, callback);
+  console.log(`Brunch development server running at http://localhost:${port}`);
+  return app;
+}
