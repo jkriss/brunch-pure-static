8c5e867ec043b61616e3be2cc06d31f8ee389f48
Jesse Kriss <jesse@jklabs.net>
2015-12-30 23:23:34 -0800

    Adding another dependency for testing

--- app/footer.js
+++ app/footer.js
@@ -1,5 +1,7 @@
+var Jklabs = require('./jklabs');
+
 module.exports = React.createClass({
   render : function() {
-    return <div>Hey, I'm a footer</div>
+    return <div>Hey, I'm a footer! <Jklabs/></div>
   }
 })
--- app/react.static.jsx
+++ app/react.static.jsx
@@ -3,6 +3,6 @@ bindings:
   '#app' : './app'
 ---
 
-<p>This is a page with React stuff in it.</p>
+<p>This is a page with React stuff in it. Fun.</p>
 
 <div id="app">app stuff goes here</div>
